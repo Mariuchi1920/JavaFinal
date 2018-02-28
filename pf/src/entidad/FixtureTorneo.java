73f67b4524ca16f5167df917337684493c335ea1
maria <maria@DESKTOP-DV8EUEU>
2018-02-28 15:37:15 -0300

    fixture corregir

---
+++ pf/src/entidad/FixtureTorneo.java
@@ -0,0 +1,12 @@
+package entidad;
+
+import java.util.LinkedList;
+
+public class FixtureTorneo {
+
+	public static void generarFixture(LinkedList<EquiposTorneos> equipoTorneo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
